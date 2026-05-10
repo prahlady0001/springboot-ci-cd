c9b81b535efe08f547063857cbfdd39ef4427edc
Prahlad <prahlady0001@gmail.com>
2026-05-10 14:39:37 +0530

    Add clas

--- src/main/java/springboot_cicd_app/config/S3Config.java
+++ src/main/java/springboot_cicd_app/config/S3Config.java
@@ -3,6 +3,7 @@ package springboot_cicd_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,9 +12,9 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-
         return S3Client.builder()
                 .region(Region.AP_SOUTH_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
--- src/main/java/springboot_cicd_app/config/S3Configs.java
+++ src/main/java/springboot_cicd_app/config/S3Configs.java
@@ -1,6 +1,7 @@
 package springboot_cicd_app.config;
 
 import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -10,6 +11,7 @@ public class S3Configs {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.AP_SOUTH_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
