494cabef3c90f048d6f4b5f7f6e7673759c2c5d6
Prahlad <prahlady0001@gmail.com>
2026-05-10 14:44:57 +0530

    comment class

--- src/main/java/springboot_cicd_app/config/S3Configs.java
+++ src/main/java/springboot_cicd_app/config/S3Configs.java
@@ -1,17 +1,17 @@
-package springboot_cicd_app.config;
-
-import org.springframework.context.annotation.Bean;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-
-public class S3Configs {
-
-    @Bean
-    public S3Presigner s3Presigner() {
-        return S3Presigner.builder()
-                .region(Region.AP_SOUTH_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-    }
-}
+//package springboot_cicd_app.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+//
+//public class S3Configs {
+//
+//    @Bean
+//    public S3Presigner s3Presigner() {
+//        return S3Presigner.builder()
+//                .region(Region.AP_SOUTH_1)
+//                .credentialsProvider(DefaultCredentialsProvider.create())
+//                .build();
+//    }
+//}
